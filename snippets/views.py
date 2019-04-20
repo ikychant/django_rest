76f11b4fb622ac8f87416da7465024f6c4d22b4c
ikychant <iki.informasi@gmail.com>
2019-04-20 13:37:40 +0700

    Use Routers.

--- snippets/views.py
+++ snippets/views.py
@@ -8,14 +8,6 @@ from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 
 
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
-
-
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     This viewset automatically provides `list` and `detail` actions.
