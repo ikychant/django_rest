ef06abbf97450b9046d1fe20ba9b90995d04d22d
ikychant <iki.informasi@gmail.com>
2019-04-20 12:30:50 +0700

    v0.4.0: Authentication & Permissions.

--- django_rest/urls.py
+++ django_rest/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('snippets.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
